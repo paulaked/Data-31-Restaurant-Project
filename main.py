34ad6ad093eb97d1a65f2fbf2cb781c3d9a9b007
gabensaw <gabriel.sawicki1995@gmail.com>
2022-07-19 14:43:10 +0100

    creating order method

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from unittest import main
 
 table01 = Table(5)
 table01.order("Risotto", 12.50, 2)
+table01.order("Risotto", 12.50, 4)
+table01.order("Burrito", 12.50, 4)
 print(table01.bill)
 table01.order("Burrito", 20.43, 3)
 table01.remove("Burrito", 20.43, 2)
--- restaurant.py
+++ restaurant.py
@@ -1,9 +1,15 @@
 class Table:
-    def __init__(self):
-        pass
+    bill = []
 
-    def order(self):
-        pass
+    def __init__(self, customers_number):
+        self.customers_number = customers_number
+
+    def order(self, item, price, quantity=1):
+        for dish in self.bill:
+            if dish['item'] == item:
+                dish['quantity'] += quantity
+                return self.bill
+        return self.bill.append({'item': item, 'price': price, 'quantity': quantity})
 
     def remove(self):
         pass
