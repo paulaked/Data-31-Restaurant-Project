35af9defe181a6d1ccc65bb06cb7fd324586e54f
Poojith Patnala (patnalap) <patnalap@coventry.ac.uk>
2022-07-19 17:53:59 +0100

    functions completed - except split bill in restaurant.py

--- restaurant.py
+++ restaurant.py
@@ -2,45 +2,54 @@ class Table:
 
     def __init__(self, dinners):
         self.dinners = dinners
-
-        self.bill = []
-
+        self.bill = [] # instance variable of bill list
 
 
+    def order(self, item, price, quantity=1):
 
-    def order(self, item, price, quantity=1): # **kwargs keyword passes arguments to a function that are assigned to a particular keyword.
+        for food_item in self.bill:
+            if(food_item["item"]) == item:
+                food_item["quantity"] = food_item["quantity"] + quantity
+                return
 
-        #list = {"item": [], "price": [], "quantity": []};
-
-        #if {"item": item, "price": price, "quantity":quantity} in bill.values():
-        #    quantity = quantity + 1
-
-        for num in self.bill:
-            if(num["item"]) == item:
-                pass
-
-        list = {"item": item, "price": price, "quantity":quantity}
-        self.bill.append(list)
-        self.bill["item"].append(item)
-        self.bill["price"].append(price)
-        self.bill["quantity"].append(quantity)
+        dict = {"item": item, "price": price, "quantity":quantity}
+        self.bill.append(dict)
 
 
     def remove(self, item, price, quantity=1):
         for food_item in self.bill:
+
             if food_item["item"] == item and food_item["price"] == price:
+
                 if (food_item["quantity"] - quantity) == 0:
                     self.bill.remove(food_item)
                     return True
-                #elif
+
+                elif (food_item["quantity"] - quantity) < 0:
+                    return False
+
+                else:
+                    food_item[quantity] = food_item[quantity] - quantity
+                    return True
+
 
 
     def get_subtotal(self):
-        pass
+        sum_of_subtotal = 0
+
+        for food_item in self.bill:
+            sum_of_subtotal = sum_of_subtotal + ((food_item["price"]) * (food_item["quantity"]))
+
+        return sum_of_subtotal
 
 
-    def get_total(self):
-        pass
+
+    def get_total(self, service_charge = 0.1):
+
+        sum_of_subtotal = self.get_sum_of_subtotal()
+        sum_of_service_charge = sum_of_subtotal * service_charge
+
+        total_cost = sum_of_subtotal + sum_of_service_charge
 
     def split_bill(self):
         pass
