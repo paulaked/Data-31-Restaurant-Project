03b59a644e62c31e7d95eb81901111710a9edc65
gabensaw <gabriel.sawicki1995@gmail.com>
2022-07-19 14:53:10 +0100

    creating remove method

--- restaurant.py
+++ restaurant.py
@@ -1,8 +1,7 @@
 class Table:
-    bill = []
-
     def __init__(self, customers_number):
         self.customers_number = customers_number
+        self.bill = []
 
     def order(self, item, price, quantity=1):
         for dish in self.bill:
@@ -11,8 +10,15 @@ class Table:
                 return self.bill
         return self.bill.append({'item': item, 'price': price, 'quantity': quantity})
 
-    def remove(self):
-        pass
+    def remove(self, item, price, quantity):
+        for dish in self.bill:
+            if dish['item'] == item:
+                dish['quantity'] -= quantity
+                if dish['quantity'] == 0:
+                    return self.bill.remove(dish)
+                else:
+                    return self.bill
+
 
     def get_subtotal(self):
         pass
