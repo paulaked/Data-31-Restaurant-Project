170b22aaecf55b7d038f7966195d4e1cc285e5f9
C1nth14 <cynthiasusan31@gmail.com>
2022-07-19 14:49:53 +0100

    git commit -m "Order method is working"

--- restaurant.py
+++ restaurant.py
@@ -1,14 +1,25 @@
 class Table:
-    def __init__(self):
-        pass
+    def __init__(self, tablenumber):
+        self.tablenumber = tablenumber
+        self.bill = []
 
-    def order(self):
-        pass
+    def order(self, item, price, quantity=1):
+        item = item
+        price = price
+        quantity = quantity
 
-    def remove(self):
-        pass
+        return self.bill.append({"item": item, "price": price, "quantity": quantity})
 
-    def get_subtotal(self):
+    def remove(self, item, price, quantity):
+        price = price
+        quantity = quantity - self.order(quantity)
+
+        if quantity == 0:
+            return False
+        if quantity >= 1:
+            return True
+
+    def get_subtotal(self,):
         pass
 
     def get_total(self):
