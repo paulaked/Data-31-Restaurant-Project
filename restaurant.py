be3187f266b89cf980a31aac17f60347e8d3e6dc
sanjay rathour <SRathour@spartaglobal.com>
2022-07-20 09:55:33 +0100

    finished first two methods

--- restaurant.py
+++ restaurant.py
@@ -1,12 +1,27 @@
 class Table:
-    def __init__(self):
-        pass
+    def __init__(self, number_of_people):
+        self.bill = []
+        self.number_of_people = number_of_people
 
-    def order(self):
-        pass
+    def order(self, item, price, quantity=1):
+        if item not in self.bill:
+            ordered_items = {"item": item, "price": price, "quantity": quantity}
+            self.bill.append(ordered_items)
+        else:
+            for i in self.bill:
+                if i.items() == item:
+                    i.keys(quantity) + 1
+        return self.bill
 
-    def remove(self):
-        pass
+    def remove(self, item, price, quantity):
+        for key in self.bill:
+            if key ["item"] == item and key ["price"] == price and quantity <key ["quantity"]:
+                key ["quantity"] -= quantity
+            elif key["item"] == item and key["price"] == price and quantity >= key ["quantity"]:
+                self.bill.remove(key)
+            else:
+                return False
+
 
     def get_subtotal(self):
         pass
