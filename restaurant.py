56bad37615de962ddb12ea70938ba2b9f70c9749
sanjay rathour <SRathour@spartaglobal.com>
2022-07-20 11:28:12 +0100

    runs with no fails

--- restaurant.py
+++ restaurant.py
@@ -24,10 +24,28 @@ class Table:
 
 
     def get_subtotal(self):
-        pass
+        subtotal = 0
+        for key in self.bill:
+            subtotal1 = key ["price"] * key ["quantity"]
+            subtotal += subtotal1
+        return subtotal
 
-    def get_total(self):
-        pass
+    def get_total(self, service_c=0.10):
+        subtotal = self.get_subtotal()
+        service_c = service_c * subtotal
+        total1 = subtotal + service_c
+        subtotal = "£{0:,.2f}".format(subtotal)
+        service_c = "£{0:,.2f}".format(service_c)
+        total1 = "£{0:,.2f}".format(total1)
+        dict = {"Sub Total" : subtotal,"Service Charge" : service_c, "Total": total1}
+
+        return dict
+
+
 
     def split_bill(self):
-        pass
+        total2 =0
+        total2 += self.get_subtotal()
+        total2 = total2/self.number_of_people
+      
+        return total2
