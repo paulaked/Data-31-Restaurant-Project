0ad6c1c293c9fc07ea5d980ae51277d1e84ca9fd
C1nth14 <cynthiasusan31@gmail.com>
2022-07-20 09:52:42 +0100

    git commit -m "Order method changed"

--- restaurant.py
+++ restaurant.py
@@ -3,24 +3,28 @@ class Table:
         self.tablenumber = tablenumber
         self.bill = []
 
-    def order(self, item, price, quantity=1):
-        item = item
-        price = price
-        quantity = quantity
+    def order(self, item:str, price, quantity=1):
 
-        return self.bill.append({"item": item, "price": price, "quantity": quantity})
+        if item not in self.bill:
+            cheque = {"item": item, "price": price, "quantity": quantity}
+            self.bill.append(cheque)
+        else:
+            for food in self.bill:
+                if food.items() == item:
+                    food.keys(quantity) + 1
+        return self.bill
 
     def remove(self, item, price, quantity):
-        price = price
-        quantity = quantity - self.order(quantity)
+        quantity = quantity - self.bill[quantity]
 
         if quantity == 0:
             return False
         if quantity >= 1:
             return True
 
-    def get_subtotal(self,):
-        pass
+    def get_subtotal(self):
+        cost_of_meal = self.orderprice * self.orderquantity
+        return cost_of_meal
 
     def get_total(self):
         pass
