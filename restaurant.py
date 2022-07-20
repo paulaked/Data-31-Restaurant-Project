37e804cff6891a5572a4c295b5d8d4fbf5ab27e3
C1nth14 <cynthiasusan31@gmail.com>
2022-07-20 10:40:52 +0100

    git commit -m "get total method is working"

--- restaurant.py
+++ restaurant.py
@@ -23,14 +23,22 @@ class Table:
             else:
                 return False
 
-    def get_subtotal(self):
-        price = self.bill["price"]
-        quantity = self.bill["quantity"]
-        cost_of_meal = price * quantity
+    def get_subtotal(self) -> float:
+        cost_of_meal = 0
+        for i in self.bill:
+            cost_of_meal += (i["price"] * i["quantity"])
         return cost_of_meal
 
-    def get_total(self):
-        pass
+    def get_total(self, service_charge=0.10):
+        sub_total = self.get_subtotal()
+        charge_rate = sub_total * service_charge
+        total = round((sub_total + charge_rate), 2)
+
+        total_dict = {"Sub Total": '£{:,.2f}'.format(sub_total),
+                      "Service Charge": '£{:,.2f}'.format(charge_rate),
+                      "Total": '£{:,.2f}'.format(total)}
+
+        return total_dict
 
     def split_bill(self):
         pass
