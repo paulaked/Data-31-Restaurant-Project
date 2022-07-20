51ad14252b2f46f69a961ceda2ee19baad1a948f
Savan Meggi <you@example.comsmeggi@spartaglobal.comgit config --global user.name Your>
2022-07-20 09:45:53 +0100

    Morning Push

--- restaurant.py
+++ restaurant.py
@@ -1,9 +1,19 @@
 class Table:
-    def __init__(self):
-        pass
 
-    def order(self):
-        pass
+    def __init__(self, num_of_people):
+        self.bill = []
+        self.num_of_people = num_of_people
+
+
+    def order(self, item = "food", price = "10.00", quantity = "1"):
+        if item not in self.list:
+            ordered = {"item": item, "price": price, "quantity": quantity}
+            self.bill.append(ordered)
+        else:
+            for i in self.bill:
+                if i.items() == item:
+                    i.keys(quantity) + 1
+        return self.bill
 
     def remove(self):
         pass
@@ -16,3 +26,7 @@ class Table:
 
     def split_bill(self):
         pass
+
+
+
+
